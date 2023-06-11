786dfaef308acae79f714be23250fd0c3a1b356b
hajun <audgkwns44@gmail.com>
2023-06-11 18:07:16 +0900

    [Update]sidebar contents are moved to inner closure

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,7 +2,28 @@ import Box from "@mui/material/Box";
 import BasicCard from "./BasicCard";
 import { useState, useEffect } from "react";
 
-export default function Sidebar({ items }) {
+export default function Sidebar() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    setItems([
+      {
+        title: "킥보드 탐색",
+        description:
+          "AVL Tree를 기반으로, 사용자 위치 기준 가장 적합한 킥보드를 탐색",
+        link: "/",
+      },
+      {
+        title: "가까운 킥보드",
+        description: "AVL Tree를 기반으로, 현재 킥보드 데이터셋 트리를 재구성",
+        link: "/sort",
+      },
+      {
+        title: "킥보드 수정",
+        description: "킥보드 데이터를 생성 또는 삭제",
+        link: "/update",
+      },
+    ]);
+  }, []);
   return (
     <Box
       sx={{
