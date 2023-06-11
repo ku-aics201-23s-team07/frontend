1e3b7376da9753af9bb46cbc664015b99e2bcc75
hajun <audgkwns44@gmail.com>
2023-06-11 18:05:44 +0900

    [Update]Routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
+import SortPage from "./Pages/SortPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<LandingPage />} />
+        <Route path="/sort" exact element={<SortPage />} />
       </Routes>
     </Router>
   );
